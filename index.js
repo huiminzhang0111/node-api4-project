8a56bf6c265bc183eb167a19ac390959553f950c
Huimin <huiminzhang0111@hotmail.com>
2021-09-30 16:39:05 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('wow getting started')
+require('dotenv').config()
+
+const { PORT } = require('./config')
+
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.use('*', (req, res, next) => {
+    res.json({
+        message: "hello from index.js"
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
